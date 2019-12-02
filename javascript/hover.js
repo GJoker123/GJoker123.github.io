ac723a5e084c881ab7fca6ad5e56937251acc6b1
GJoker <53223454+GJoker123@users.noreply.github.com>
2019-12-02 19:10:19 +0000

    Revert "making some changes"
    This reverts commit 87a3318b0e5364b6fcf14fc066d9b92557c13e10.

--- javascript/hover.js
+++
@@ -1,7 +0,0 @@
-function onhover() {
-    document.getElementById("eyechange").src = ("images/opened_eye_for_crypt.png");
-    setTimeout(function () {
-        window.location.href = "story.html"
-    }, 2000)
-};
-
